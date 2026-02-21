467b7b118f76d892e160aeb14184a4a4c3100c22
Jaivesh8 <jaivesh50@gmail.com>
2026-02-21 17:59:04 +0530

    aith fixes

--- app/db/database.py
+++ app/db/database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "sqlite:///./reelforge.db"
-
+import os
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+   pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
