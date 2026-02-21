0df5eadced9cbb0981e5f7aaab9d03eab93d9944
Jaivesh8 <jaivesh50@gmail.com>
2026-02-21 17:19:59 +0530

    added auth and profile

---
+++ app/profiles/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+class ProfileCreateUpdate(BaseModel):
+    display_name: str
+    creator_type: str
+    organization_type: Optional[str] = None
+    experience_level: Optional[str] = None
+    platform:str
+    audience_type:str
+    goals: Optional[str] = None
+    niches: List[str] = []
+
+class ProfileResponse(BaseModel):
+    display_name: str
+    creator_type: str
+    organization_type: Optional[str]
+    experience_level: Optional[str]
+    platform:str
+    audience_type:str
+    goals: Optional[str]
+    niches: List[str]
